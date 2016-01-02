070dc53f6c1d08921a76cf1f3963141e66855603
gongsongping <gongsongping@gmail.com>
2016-01-02 15:03:52 +0700

    directivesngstyle

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,45 +1,62 @@
 angular.module('starter.directives', [])
 angular.module('starter.directives')
-.directive('chsquare', function () {
+.directive('bodywidth', function ($rootScope) {
   return {
     restrict: 'A',
     scope: {},
-    link: function(scope, elem, attrs) {
-      var width = elem.css('width')
-      scope.getWidth = function () {
-        return document.getElementById('chcontainer').clientWidth
-      }
-      scope.$watch(scope.getWidth, function (width) {
-        console.log((1/3)*width + 'px')
-        elem.css("float", "left")
-        elem.css('height', (1/3)*width + 'px')
-        elem.css('width', (1/3)*width + 'px')
-      })
-      // var width = document.getElementById('container').clientWidth
-      // console.log(width)
-      // elem.on('load', function(){console.log(elem.width())})
-      // elem.css('height', '160px')
+    link: function(scope, element, attrs) {
+      if (document.body.clientWidth >= 768){
+         $rootScope.containerwidth = 768
+       } else {
+         $rootScope.containerwidth = document.body.clientWidth
+       }
+      console.log(window.innerWidth)
+      console.log(document.body.clientWidth)
+      console.log($rootScope.containerwidth)
     }
   }
 })
-.directive('fsquare', function () {
-  return {
-    restrict: 'A',
-    scope: {},
-    link: function(scope, elem, attrs) {
-      var width = elem.css('width')
-      scope.getWidth = function () {
-        return document.getElementById('fcontainer').clientWidth
-      }
-      scope.$watch(scope.getWidth, function (width) {
-        console.log((1/3)*width + 'px')
-        elem.css("float", "left")
-        elem.css('height', (1/3)*width + 'px')
-        elem.css('width', (1/3)*width + 'px')
-      })
-    }
-  }
-})
+
+// .directive('chsquare', function () {
+//   return {
+//     restrict: 'A',
+//     scope: {},
+//     link: function(scope, elem, attrs) {
+//       var width = elem.css('width')
+//       scope.getWidth = function () {
+//         return document.getElementById('chcontainer').clientWidth
+//       }
+//       scope.$watch(scope.getWidth, function (width) {
+//         console.log((1/3)*width + 'px')
+//         elem.css("float", "left")
+//         elem.css('height', (1/3)*width + 'px')
+//         elem.css('width', (1/3)*width + 'px')
+//       })
+//       // var width = document.getElementById('container').clientWidth
+//       // console.log(width)
+//       // elem.on('load', function(){console.log(elem.width())})
+//       // elem.css('height', '160px')
+//     }
+//   }
+// })
+// .directive('fsquare', function () {
+//   return {
+//     restrict: 'A',
+//     scope: {},
+//     link: function(scope, elem, attrs) {
+//       var width = elem.css('width')
+//       scope.getWidth = function () {
+//         return document.getElementById('fcontainer').clientWidth
+//       }
+//       scope.$watch(scope.getWidth, function (width) {
+//         console.log((1/3)*width + 'px')
+//         elem.css("float", "left")
+//         elem.css('height', (1/3)*width + 'px')
+//         elem.css('width', (1/3)*width + 'px')
+//       })
+//     }
+//   }
+// })
 .directive('imgsquare', function () {
   return {
     restrict: 'A',
