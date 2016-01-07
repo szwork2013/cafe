cc5c3dbae52dd9262c2c1558ca02f3942c7be137
gongsongping <gongsongping@gmail.com>
2016-01-07 23:49:29 +0700

    dangkyiconchange

--- www/js/app.js
+++ www/js/app.js
@@ -7,9 +7,9 @@
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'starter.directives', 'ngResource','ngCordova','ngFileUpload']) //'firebase',
 
 .run(function($ionicPlatform, $http, $window, $rootScope, $state, $resource) {
-  $rootScope.baseUrl = "http://localhost:3000"
+  // $rootScope.baseUrl = "http://localhost:3000"
   //  $rootScope.baseUrl = "http://162.243.143.15"
-  // $rootScope.baseUrl = "http://changiif.com"
+  $rootScope.baseUrl = "http://changiif.com"
   $resource('http://changiif.com/uptoken').get().$promise.then(function(data) {
     $window.localStorage.qiniuToken = data.uptoken
     console.log('qiniuT  ' + $window.localStorage.qiniuToken)
